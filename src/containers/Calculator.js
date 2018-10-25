264d00e1309950c83d90c9500f9a87af46d16a10
hazelGranger <wangzhen_blcu@163.com>
2018-10-25 21:56:25 +1300

    change looks

--- src/containers/Calculator.js
+++ src/containers/Calculator.js
@@ -14,26 +14,28 @@ class Calculator extends Component {
           <Expression />
           <Result />
         </div>
-        <Number value={"7"} />
-        <Number value={"8"} />
-        <Number value={"9"} />
-        <Operator value="+" />
-        <Number value={"4"} />
-        <Number value={"5"} />
-        <Number value={"6"} />
-        <Operator value="-" />
-        <Number value={"1"} />
-        <Number value={"2"} />
-        <Number value={"3"} />
-        <Operator value="x" />
-        <Number value={"0"} />
-        <Number value="." />
-        <Number type="signChanger" value="±" />
-        <Operator value="÷" />
-        <ClearOperator />
-        <Number value="" />
-        <Number value="" />
-        <Operator value="=" />
+        <div className="Buttons">
+          <Number value={"7"} />
+          <Number value={"8"} />
+          <Number value={"9"} />
+          <Operator value="+" />
+          <Number value={"4"} />
+          <Number value={"5"} />
+          <Number value={"6"} />
+          <Operator value="-" />
+          <Number value={"1"} />
+          <Number value={"2"} />
+          <Number value={"3"} />
+          <Operator value="x" />
+          <Number value={"0"} />
+          <Number value="." />
+          <Number type="signChanger" value="±" />
+          <Operator value="÷" />
+          <ClearOperator />
+          <Number value="" />
+          <Number value="" />
+          <Operator value="=" />
+        </div>
       </div>
     );
   }
